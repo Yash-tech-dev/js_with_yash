53c1dbd70882a9e1c8fb5ca81352e7a5b7dc4a2a
Yash Saini <sainiyash66777@gmail.com>
2025-08-12 16:45:53 +0000

    topics till now

---
+++ 01_basics/accountid.js
@@ -0,0 +1,6 @@
+const accountid = 101;
+let accountEmail = "yashsaini@1234gmail.com";
+var accountPassword = "Yash@123";
+accountcity = "kanpur";
+console.log(accountPassword);
+console.log(accountcity);
